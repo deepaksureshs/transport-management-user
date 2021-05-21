3797ebb13ab22d2b4754c3840b5d577e0d7521b1
Deepak Suresh S <deepaksuresh10@gmail.com>
2021-05-21 19:31:51 +0530

    Initial commit

---
+++ src/main/java/com/company/transportmanagementuser/TransportManagementUserApplication.java
@@ -0,0 +1,13 @@
+package com.company.transportmanagementuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportManagementUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportManagementUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/transportmanagementuser/TransportManagementUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.transportmanagementuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportManagementUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
